3a28245732bc0dc2980b62428232a09627286673
fdarcy <fdarcy@mail.ru>
2024-01-17 19:15:13 +0300

    web

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -5,12 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CardTest {
     private WebDriver driver;
